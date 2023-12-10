78fac28e77fa74fafb06ac9ce0046b579878eed1
Sergio Nhaca <snhaca@hotmail.com>
2023-12-10 20:42:19 +0200

    feat: insert antd and styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 
+import { Button } from 'antd';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 import viteLogo from '/vite.svg';
 
@@ -10,7 +12,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <StyledLink>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,14 +23,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </StyledLink>
   );
 }
+
+const StyledLink = styled.div`
+  color: red;
+  font-weight: bold;
+`;
 
 export default App;
